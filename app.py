a8f966a49b9cff787ff519c75c97e9fad96712ec
Steven Rey <stevenreybritez@gmail.com>
2025-06-04 10:48:01 -0300

    modified https

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from fastapi import FastAPI
 import uvicorn
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 app = FastAPI()
+
+# Agregar middleware para redirección HTTPS
+app.add_middleware(HTTPSRedirectMiddleware)
 
 @app.get("/")
 async def read_root():
     return {"message": "Hola Mundo"}
 
 if __name__ == "__main__":
-    uvicorn.run(
+    # Configurar el servidor HTTPS
+    config = uvicorn.Config(
         "app:app",
         host="kuarahy.deportivosajonia.lan",
         port=443,
         ssl_keyfile="./certs/private.key",
         ssl_certfile="./certs/certificate.crt"
     )
+    server = uvicorn.Server(config)
+    server.run()
