412b5af3c0feb7842b152123d3c0f2645c84bd80
Steven Rey <stevenreybritez@gmail.com>
2025-06-04 11:18:29 -0300

    modified host and port local

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ async def read_root():
     return {"message": "Hola Mundo"}
 
 if __name__ == "__main__":
-    config = uvicorn.Config(
+    uvicorn.run(
         "app:app",
         host="127.0.0.1",
         port=8000,
+        ssl_keyfile="./certs/private.key",
+        ssl_certfile="./certs/certificate.crt"
     )
-    server = uvicorn.Server(config)
-    server.run()
